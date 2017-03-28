d16b5b26d99b91cb63e49a2503337fb83c8f4f6e
Luan Andryl <luan.andryl@gmail.com>
2017-03-28 12:46:32 -0300

    team page added

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <home></home>
     <about></about>
+    <team></team>
   </div>
 </template>
 
 <script>
 import home from './components/home';
 import about from './components/about';
+import team from './components/team';
 
 export default {
   name: 'app',
   components: {
     home,
     about,
+    team,
   },
 };
 </script>
