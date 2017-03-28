7585f2b4ac61af52eb698983273ace1b8f4703dc
Luan Andryl <luan.andryl@gmail.com>
2017-03-28 10:04:33 -0300

    home component added

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import home from './components/home';
+
+export default {
+  name: 'app',
+  components: {
+    home,
+  },
+};
+</script>
+
+<style lang='sass'>
+</style>
